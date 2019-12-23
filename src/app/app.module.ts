fc7d0e2479a6508d072ffd1db6fb4b8ff5fc0464
alfonsserra <alfonsoserra@gmail.com>
2019-12-23 08:39:17 +0100

    Repackage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ReplaySubjectModule } from './modules/replay-subject/replay-subject.module';
+import { ReplaySubjectModule } from './features/replay-subject/replay-subject.module';
 import { ToolbarComponent } from '@components/toolbar/toolbar.component';
 
 
